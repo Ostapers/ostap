35f77fb4d440ba4124b602a5d9ee80b73a4239ef
kakadu <kakadu.hafanana@gmail.com>
2013-10-20 15:45:46 +0400

    Initial commit

---
+++ Makefile
@@ -0,0 +1,25 @@
+.PHONY: all test install uninstall clean
+
+#VERBOSE=-verbose 3
+TESTS=001 002 003 004 005 006 007 008 009 010 011 012
+TEST_TARGETS=$(addsuffix .byte, $(addprefix test/test,$(TESTS)) )
+INSTALL_TARGETS=pa_ostap.cmo ostap.cm[oix] ostap.o lib/*.mli util/*.mli
+
+all:
+	ocamlbuild -use-ocamlfind lib/ostap.cmo lib/ostap.cmx $(VERBOSE)
+	ocamlbuild -use-ocamlfind -Is lib pa_ostap.cmo $(VERBOSE)
+	ocamlbuild -use-ocamlfind util/util.cmo util/util.cmx
+	ocamlbuild -use-ocamlfind -Is lib -Is util ostap.cmo ostap.cmx
+
+test: 
+	ocamlbuild -use-ocamlfind $(TEST_TARGETS) $(VERBOSE)
+
+install: 
+	cd _build && ocamlfind install ostap ../META $(INSTALL_TARGETS)
+
+uninstall:
+	ocamlfind remove ostap
+
+clean:
+	rm -fr _build test0*.byte
+
---
+++ lib/msg.ml
@@ -0,0 +1,109 @@
+(*
+ * Msg: parsing message module.
+ * Copyright (C) 2006
+ * Dmitri Boulytchev, St.Petersburg State University
+ * 
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License version 2, as published by the Free Software Foundation.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * See the GNU Library General Public License version 2 for more details
+ * (enclosed in the file COPYING).
+ *)
+
+open Printf
+
+module Coord =
+  struct
+
+    type t = int * int
+
+    let line = fst
+    let col  = snd
+
+    let toString (x, y) = sprintf "(%d:%d)" x y 
+
+    let compare : t -> t -> int = fun (r, c) (r', c') ->
+      let x = compare r r' in
+      if x = 0 then compare c c' else x
+   
+  end
+
+module Locator =
+  struct
+
+    type t = No | Point of Coord.t | Interval of Coord.t * Coord.t | Set of t list
+    and  l = t
+
+    let makeInterval x y =
+      match x, y with
+      | Point x, Point y -> Interval (x, y)
+      | _ -> Set [x; y]
+
+    let rec toString = function
+      | No              -> ""
+      | Point x         -> Coord.toString x 
+      | Interval (x, y) -> sprintf "%s-%s" (Coord.toString x) (Coord.toString y)
+      | Set      x      -> 
+	  let module M = View.List (struct type t = l let toString = toString end) in
+          M.toString x
+
+    let compare x y =
+	match (x, y) with
+	| No, No -> 0
+	| No, _  -> -1
+	| _ , No -> 1
+	| _ ->
+	    let rec least = function
+	      | No              -> (0, 0)
+	      | Point     x     -> x
+	      | Interval (x, _) -> x
+	      | Set       x     -> List.hd (List.sort Coord.compare (List.map least x))
+	    in
+	    Coord.compare (least x) (least y)
+
+  end
+
+type t = {phrase: string; args: string array; loc: Locator.t} 
+
+let make      phrase args loc = {phrase=phrase; args=args; loc=loc}
+let loc       t               = t.loc
+
+let phrase    phrase          = make phrase [||] Locator.No
+let orphan    phrase args     = make phrase args Locator.No
+
+let string t = 
+  let parmExpr = Str.regexp "%\\([0-9]+\\)" in
+  Str.global_substitute 
+    parmExpr  
+    (fun s -> 
+      try 
+        t.args.(int_of_string (Str.replace_matched "\\1" s))
+      with
+      | Failure "int_of_string" -> 
+          raise (Failure 
+                   (sprintf "invalid integer parameter specification in message phrase \"%s\"" s)
+                )
+	    
+      | Invalid_argument "index out of bounds" ->
+          raise (Failure 
+                   (sprintf "index out of bound while accessing message parameter in \"%s\"" s)
+                )
+    )
+    t.phrase
+    
+let toString t =
+  let message = string t in
+    match Locator.toString t.loc with
+    | ""  -> message
+    | loc -> message ^ " at " ^ loc
+      
+let augment msg loc = match msg.loc with Locator.No -> {msg with loc = loc} | _ -> msg
+let augmentList msgs loc = List.map (fun x -> augment x loc) msgs
+
+let extend msg str = {msg with phrase=str ^ msg.phrase}
+let extendList msgs str = List.map (fun msg -> extend msg str) msgs
